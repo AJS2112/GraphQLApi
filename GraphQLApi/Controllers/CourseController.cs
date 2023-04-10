a9f87d737d462b6b7137663b376f60507fad58ec
Antonio Jose Sucre Gonzalez <antonio.gonzalez@efrete.com.br>
2023-04-10 19:46:33 -0300

    Adicionar arquivos de projeto.

---
+++ GraphQLApi/Controllers/CourseController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GraphQL.Types;
+
+namespace GraphQLApi.Controllers
+{
+    [Route("graphql")]
+    [ApiController]
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
